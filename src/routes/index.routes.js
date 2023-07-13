06906b5d27cd7460d3d3d871f3d44654e5a71fdc
breno.breves <brenocsb@dcc.ufrj.br>
2023-07-13 17:21:27 -0300

    feat/buscar por produto por ID ajuste do router

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import usuariosRouter from "./usuarios.routes.js";
-import prodRouter from "./produtos.routes.js";
+import produtosRouter from "./produtos.routes.js";
 
 const router = Router();
 
 router.use(usuariosRouter);
+router.use(produtosRouter);
 
 export default router;
