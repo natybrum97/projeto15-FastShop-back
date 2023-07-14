6b88d29516ba15fefa4bb46b2be016aa83079065
Nathália Brum <natybrum.97@gmail.com>
2023-07-14 05:07:59 -0300

    feat: Versão com middlewares

--- src/routes/produtos.routes.js
+++ src/routes/produtos.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { getProdID } from "../controllers/produtos.controller.js";
 import { pegarProdutos } from "../controllers/produtos.controller.js";
+import { validateAuth } from "../middlewares/validateAuth.js";
 
 const produtosRouter = Router();
 
-produtosRouter.get("/produtos/:id", getProdID);
-produtosRouter.get("/catalogo", pegarProdutos );
+produtosRouter.get("/produtos/:id", validateAuth, getProdID);
+produtosRouter.get("/catalogo", validateAuth, pegarProdutos );
 
 export default produtosRouter;
 
