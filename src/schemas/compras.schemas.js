3f1c5bd5150b91c5151ef600c3d523a81e44ac77
breno.breves <brenocsb@dcc.ufrj.br>
2023-07-17 16:39:11 -0300

    Ajustes finais do registro de compra

--- src/schemas/compras.schemas.js
+++ src/schemas/compras.schemas.js
@@ -6,5 +6,17 @@ export const schemaCompra = Joi.object({
     valor: Joi.number().required(),
     parcelas: Joi.number().min(1).max(12).required(),
     tipo: Joi.string().valid("Boleto","Cartão de Crédito").required(),
-    ccnumber: Joi.string().creditCard()
+    ccnumber: Joi.string().creditCard(),
+    cvv: Joi.number().min(3),
+    expirationDate: Joi.date(),
+    nomeCartao: Joi.string(),
+    nomeCompleto: Joi.string().required(),
+    telefone: Joi.string().min(10).required(),
+    cep: Joi.string().min(8).max(8).required(),
+    rua: Joi.string().required(),
+    numeroCasa: Joi.number().required(),
+    state: Joi.string().required(),
+    cidade: Joi.string().required(),
+    bairro: Joi.string().required(),
+    cpf: Joi.string().min(11).max(11).required()
 })
